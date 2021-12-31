fb53899e5701baf5de91536ca14f64902863b404
prabhusiva92 <sjprabhu91@gmail.com>
2021-12-31 16:52:17 +0530

    Folder Structure

---
+++ src/org/test/FacebookMobile.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class FacebookMobile {
+	
+	private void login() {
+		System.out.println("Welcome to Facebook");
+
+	}
+	public static void main(String[] args) {
+		FacebookMobile fb = new FacebookMobile();
+		fb.login();
+	}
+
+}
